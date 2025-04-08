0e95aa2e3f5551d5bd90a6761a3e6701e099bbff
axs-seahorse7 <axs.sh7@gmail.com>
2025-04-08 14:41:42 +0530

    update vit-config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   server: {
     host: true,
-    allowedHosts: [""],
+    allowedHosts: ["frustratinggame.onrender.com"],
     port: 5173,  
     strictPort: true,  
     cors: true,
