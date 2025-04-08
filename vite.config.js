b63cec61728a1510b6ac77c39cb8f67ef1587313
axs-seahorse7 <axs.sh7@gmail.com>
2025-04-08 14:37:14 +0530

    update vite-config

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,10 @@ export default defineConfig({
 
   ],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts:["all"]
+    host: true,
+    allowedHosts: [""],
+    port: 5173,  
+    strictPort: true,  
+    cors: true,
   },
 })
